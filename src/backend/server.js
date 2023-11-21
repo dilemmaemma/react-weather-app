25b89bf05e363f36315bb683ccbf84c90376ef08
delimmaemma <calvinhtml@gmail.com>
2023-11-20 19:41:27 -0600

    Initial Commit

---
+++ src/backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+      message: err.message,
+      stack: err.stack,
+    });
+  });
+
+module.exports = server
